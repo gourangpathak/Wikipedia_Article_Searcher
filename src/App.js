df04922d18da96cc7cbad9758f02b7044da432d6
gourangpathak <gourangp29@gmail.com>
2022-05-08 00:02:39 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Search from "./components/Search";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui grid container center aligned">
+        <div className="column eight wide">
+        <Search/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,25 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+const List = ({ results }) => {
+    const renderedList = results.map((item) => {
+        return (
+            <div key={item.pageid} className="ui segment">
+                <h2>
+                    <a
+                     href={"https://en.wikipedia.org?curid=" + item.pageid}
+                     className="header"
+                     target="_blank"
+                     rel="noreferrer"
+                     > {item.title}
+                     </a>
+                </h2>
+                <p dangerouslySetInnerHTML={{__html: item.snippet}}></p>
+            </div>
+        );
+    });
+    return renderedList;
+};
+
+export default List;
